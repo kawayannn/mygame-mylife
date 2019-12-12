68ac3fed0c8823231c5a6b869cf356ef7b117c4c
Kawata Seiya <kawataseiya@yahoo.co.jp>
2019-12-12 21:29:04 +0900

    destroy action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,6 +13,18 @@ class RelationshipsController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:relationship][:follow_id])
+    following = current_user.unfollow(user)
+    if following.destroy
+      flash[:success] = 'フォローを解除しました'
+      redirect_to user
+    else
+      flash.now[:alert] = 'フォローの解除に失敗しました'
+      redirect_to user
+    end
+  end
+
   private
 
   def set_user
