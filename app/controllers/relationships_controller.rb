a6f74f7096c5c9e24ca3daa0d23b174476a96e60
Kawata Seiya <kawataseiya@yahoo.co.jp>
2019-12-12 21:23:00 +0900

    create action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,18 @@
 class RelationshipsController < ApplicationController
   before_action :set_user
 
+  def create
+    user = User.find(params[:relationship][:follow_id])
+    following = current_user.follow(user)
+    if following.save
+      flash[:success] = 'フォローしました'
+      redirect_to user
+    else
+      flash.now[:alert] = 'フォローに失敗しました'
+      redirect_to user
+    end
+  end
+
   private
 
   def set_user
