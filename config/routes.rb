25249e30ebd5ad04260ce2d40ecc036c4907243c
Kawata Seiya <kawataseiya@yahoo.co.jp>
2019-12-15 22:22:17 +0900

    rooting

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
   resources :users,         only:[:index, :show, ]
-  root to:'articles#index'
+  root to:'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
