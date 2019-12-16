952b01f77838d82822dfca786ca9bbbd78a0c119
Kawata Seiya <kawataseiya@yahoo.co.jp>
2019-12-16 19:49:40 +0900

    carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :articles
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
+  # mount_uploader :image
 
   def follow(other_user)
     unless self == other_user
