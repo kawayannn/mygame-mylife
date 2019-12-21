59aed63e3758f66e7048e8d21040311041c0b7c6
Kawata Seiya <kawataseiya@yahoo.co.jp>
2019-12-22 01:10:47 +0900

    group.rb association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,7 @@
 class Group < ApplicationRecord
+  has_many :users
+  has_many :messages
+
+  has_many :group_gametitles, dependent: :destroy
+  has_many :gametitles, through: :group_gametitles
 end
