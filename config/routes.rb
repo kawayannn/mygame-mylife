b3114ba0e879a428371e7db7f139b881fc80bc41
Kawata Seiya <kawataseiya@yahoo.co.jp>
2019-12-22 01:44:04 +0900

    form.haml text_area_title

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :groups
   devise_for :users
   resources :articles do
     resources :comments,      only:[:create] 
   end
+  resources :groups,        only:[:new, :create, :index]
   resources :users,         only:[:index, :show] 
   root to:'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
