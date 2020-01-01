8eb9aa57186c7e99e0bd0efb33a6876e0876d248
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-01-02 00:59:03 +0900

    form-build

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,15 +3,31 @@ window.addEventListener("load", function() {
   group_id = $("#group-header__right").data('group-id');
 
   function formHTML(group){
-    var html = `<form class="new_message" id="new_message" action="/groups/${group_id}/messages" accept-charset="UTF-8" method="post">
-                  <input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="gO0bQLrR4G1nTEVNHEULfiDge6db6kSjHDFyoJ+MliQdlGWfMpbhXD8JOp/C0h12sSGzwVXK1OlPfAnheYoVSg==">
-                    <textarea name="message[content]" id="message_content">
-                    </textarea>
-                  <input type="submit" name="commit" value="送信" data-disable-with="送信">
-                </form>`
+    var html = `<div class="build-form shadow-lg p-3 mb-5 bg-white rounded">
+                  <form class="new_message" id="new_message" action="/groups/${group_id}/messages" accept-charset="UTF-8" method="post">
+                    <div class="new_message__uper">
+                      <button type="button" class="close" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token">
+                      <textarea placeholder="メッセージを入力してください" name="message[content]" id="message_content"></textarea>
+                    <div class="new_message__lower">
+                      <div class="new_message__lower--right">
+                        <button type="submit" class="btn btn-primary btn-sm" name="commit" data-disable-with="送信">
+                          送信
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>`
     return html
   }
   btn.addEventListener("click", function() {
     $("main.group").append(formHTML);
   });
+
+  $(document).on("click", ".close", function(){
+    $(".build-form").remove();
+  })
 });
