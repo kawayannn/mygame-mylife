b02f701c8bd9da98277019313eb368fc970ec53d
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-01-04 19:50:35 +0900

    mount_uploder

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
-  has_many :article_gametitles
+  has_many :article_gametitles,dependent: :destroy
   has_many :gametitles, through: :article_gametitles
   
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+
+  mount_uploader :image, ArticleThumbnailsUploader
 end
