b4364492a00e363c945d1b1e296b10489536997c
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-01-09 01:12:02 +0900

    gem counter_culture

--- app/models/user_gametitle.rb
+++ app/models/user_gametitle.rb
@@ -1,4 +1,5 @@
 class UserGametitle < ApplicationRecord
   belongs_to :user
   belongs_to :gametitle
+  counter_culture :gametitle, column_name: "user_gametitle_count", touch: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_114541) do
+ActiveRecord::Schema.define(version: 2020_01_08_124755) do
 
   create_table "article_gametitles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "article_id", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_01_06_114541) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_gametitle_count", default: 0, null: false
   end
 
   create_table "group_gametitles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
