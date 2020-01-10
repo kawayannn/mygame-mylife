1164f54eecfb9dde595c02a4cd0011a515ead010
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-01-10 20:46:41 +0900

    join?
    グループに参加しているユーザーには「メッセージ作成ボタン」を表示
    不参加のユーザーには「グループに参加ボタン」を表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,5 +8,9 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
 
   mount_uploader :image, GroupImageUploader
+
+  def join?(user_id)
+    self.user_ids.include?(user_id)
+  end
   
 end
