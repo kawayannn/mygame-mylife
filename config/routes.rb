e161152842a7bc800411bf883909f35f36bdd731
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-01-19 21:18:21 +0900

    follow_btn
    フォローボタンを作成
    配置はまだ

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,7 @@ Rails.application.routes.draw do
       get :search
     end
   end
+
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
