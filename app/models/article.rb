c78d0d5fcda2d0d6a620ac69232419555331a09a
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-02-11 20:07:14 +0900

    validate

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,10 @@ class Article < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
+  validates :title, :content, presence: true
+  validates :title, length: {maximum: 40}
+  validates :content, length: {maximum: 1000}
+
   mount_uploader :image, ArticleThumbnailsUploader
   is_impressionable counter_cache: true
   
